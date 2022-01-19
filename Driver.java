2594d7d13a1d0b680d234554381ed5c53fb636fc
WalterCruz1600 <72421497+WalterCruz1600@users.noreply.github.com>
2022-01-19 12:50:34 -0600

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -17,6 +17,9 @@ public class Driver {
         desplegarMenu();
     }
 
+    /**
+     * Se despliega el menú con todas las opciones que se pueden hacer en el radio
+     */
     private static void desplegarMenu() {
         int opcion = 0;
 		while (opcion != 7){
@@ -24,7 +27,7 @@ public class Driver {
 			System.out.println("\n\n Escoja una opcion del menú");
 			System.out.println("1. Prende el radio");
 			System.out.println("2. Cambia de AM a FM/ de FM a AM");
-			System.out.println("3. Avanzar en el dial de las emisoras. Al llegar al final del dial inicia nuevamente.");
+			System.out.println("3. Mover en el dial de las emisoras. Al llegar al final del dial inicia nuevamente.");
 			System.out.println("4. Guardar una emisora en uno de los 12 botones            ");
 			System.out.println("5. Selecciona una de las emisoras guardadas");
 			System.out.println("6. Apagar el radio");
@@ -35,6 +38,7 @@ public class Driver {
 			try{opcion = scan.nextInt();}
 			catch(Exception e){System.out.println("INVALIDO");}
 			//catch(Exception e){System.out.println("INVALIDO");}
+
 
 			if (opcion == 1){prender();}
             else if(opcion ==2){cambiarAM_FM();}
@@ -54,11 +58,19 @@ public class Driver {
      */
     private static void prender() {
         boolean on = r.isOn();
+        boolean isAM = r.getFrequency();
+        double station=  r.getStation();
         if(on) {
             System.out.println("La radio ya se encuentra encendida");
         }else {
             r.turnOnOff();
             System.out.println("Se ha encendido la radio");
+            if(isAM){
+                System.out.println("Usted se encuentra en la emisora "+station+" AM");
+            }else{
+                System.out.println("Usted se encuentra en la emisora "+station+" FM");
+            }
+            
         }
     }
 
@@ -81,6 +93,7 @@ public class Driver {
      * Solo acepta numeros del 1-12
      */
     private static void guardarEmisora() {
+        
         if (r.isOn()) {
             double station = r.getStation();
         System.out.println("Elija el botón (1-12) para guardar la emisora "+station);
@@ -94,33 +107,137 @@ public class Driver {
                System.out.println("Elija un entero del 1-12"); 
             }
 		}
-
-        r.saveStation(position, station);
+        if(r.getFrequency()){
+            r.saveStation(position-1, station);
+        }else{
+            r.saveStation(position+12-1, station);
+        }
+        
         System.out.println("Se ha guardado la emisora "+station+" en el boton "+position);
         } else {
             System.out.println("La radio se encuentra apagada");
         }
     }
 
-
+        
+    /**
+     * Solicita el botón donde se encuentra la emisora guardada y la reproduce
+     * solo acepta de 1-12
+     */
     private static void seleccionarEmisora() {
         //TODO averiguar si no hace falta un getStation
+
+        if (r.isOn()) {
+            double station = r.getStation();
+            if(r.getFrequency()){
+                System.out.println("Usted está en frecuencia AM");
+            }else{
+                System.out.println("Usted está en frecuencia FM");
+            }
+        System.out.println("Elija el botón (1-12) donde está la emisora que desea escuchar");
+        int position = 0;
+        while (!(0<position && position <13)){
+			scan = new Scanner(System.in);
+			try{position = scan.nextInt();}
+			catch(Exception e){System.out.println("Escoja un entero");}
+
+            if (!(0<position && position <13)) {
+               System.out.println("Elija un entero del 1-12"); 
+            }
+		}
+
+        if(r.getFrequency()){
+            station=r.getSavedStation(position-1);
+        }else{
+            station=r.getSavedStation(position+12-1);
+        }
+
+        System.out.println("La emisora "+station+" está en el boton "+position+" y ahora esta sonando");
+        } else {
+            System.out.println("La radio se encuentra apagada");
+        }
     }
 
-
+    /**
+     * Mueve Dial adelante y para atrás dependiendo de la frecuencia 
+     * si llega al máximo o al mínmio regresa a la primera o a la última
+     */
     private static void avanzarDial() {
-        r.nextStation(r.getFrequency());// ??????????
-    }
+        
+        if(r.isOn()){
 
-    
-    private static void cambiarAM_FM() {
-        r.switchAMFM();
+
+            System.out.println("Usted desea mover la emisora:\n1.Adelante\n2.Atrás");
+            scan = new Scanner(System.in);
+            int dial=scan.nextInt();
+            if(dial==1){
+
+                r.nextStation(r.getFrequency());// ??????????
+                double station=  r.getStation();
+                boolean isAM = r.getFrequency();
+                if (isAM) {
+                    System.out.print("Se ha avanzado a la emisora "+station+" AM");
+                    
+                } else {
+                    System.out.println("Se ha avanzado a la emisora "+station+" FM");
+                    
+                }
+            }else if(dial==2){
+            retrocederDial();
+            }else{
+                System.out.println("Opcion invalida");
+            }
+
+
+
+        }else{
+            System.out .println("La radio se encuetra apagada");
+        }
+        
+
+        
+
+        
+    }
+    /**
+     * retrocede el Dial dependiendo de la frecuencia
+     * si llega al mínimo se envía a la estación más alta
+     */
+    private static void retrocederDial() {
+        r.prevStation(r.getFrequency());// ??????????
+        double station=  r.getStation();
         boolean isAM = r.getFrequency();
         if (isAM) {
-            System.out.println("Se ha cambiado a la frecuencia AM");
+            System.out.print("Se ha avanzado a la emisora "+station+" AM");
         } else {
-            System.out.println("Se ha cambiado a la frecuencia FM");
+            System.out.print("Se ha avanzado a la emisora "+station+" FM");
         }
+        
+    }
+
+    /**Se cambia de frecuencia */
+    private static void cambiarAM_FM() {
+        
+        if(r.isOn()){
+
+            r.switchAMFM();
+            boolean isAM = r.getFrequency();
+            if (isAM) {
+                System.out.println("Se ha cambiado a la frecuencia AM");
+                double station=r.getStation();
+                System.out.println("La estación que escuchas es "+station);
+            } else {
+                System.out.println("Se ha cambiado a la frecuencia FM");
+                double station=r.getStation();
+                System.out.println("La estación que escuchas es "+station);
+            }
+
+        }else{
+            System.out.println("La radio se encuetra apagada");
+        }
+        
+        
+        
     }
 
     
--- MiRadio.java
+++ MiRadio.java
@@ -10,58 +10,132 @@ public class MiRadio implements Radio{
     
     boolean on = false;
     boolean frequency = false;
-    double station;
-    double[] savedStationsAM = {530,540,550,560,570,580,590,600,610,620,630,640};
+    double stationAM=530;
+    double stationFM=87.9;
+    //Aquí se encuentran los botones para guardar emisoras
+    //de la posición 0-11 son AM y de la 12-23 son FM
+    double[] savedStations = {530,530,530,530,530,530,530,530,530,530,530,530,87.9,87.9,87.9,87.9,87.9,87.9,87.9,87.9,87.9,87.9,87.9,87.9};
 
     @Override
     public boolean isOn() {
-                
+     //Este método solo indica si la radio está encendida o no
         return on;
     }
 
     @Override
     public void turnOnOff() {
+        //Este método cambia el estado de encendido a apagado y viceversa
         on= !on;        
     }
 
     @Override
-    public void nextStation(boolean station) {
+    public void nextStation(boolean frecuency) {
         // TODO Auto-generated method stub
+
+        if(frecuency){
+            //Si es AM entonces avanza a la siguiente emisora con +10
+            stationAM = stationAM+10;
+            //Si sube más de la estación máxima entonces cambia a la emisora más baja 
+            if(stationAM>1610){
+                stationAM=530;
+            }
+        }else{
+            //Si es FM entonces avanza a la siguiente emisora con +0.2
+            stationFM = stationFM+0.2;
+            //Si sube más de la estación máxima entonces cambia a la emisora más baja 
+            if(stationFM>107.9){
+                stationFM=87.9;
+            }
+        }
         
     }
 
     @Override
-    public void prevStation(boolean station) {
+    public void prevStation(boolean frecuency) {
         // TODO Auto-generated method stub
+        
+        if(frecuency){
+            //Si es AM entonces retrocede a la emisora anterior con -10
+            stationAM = stationAM-10;
+            //Si baja más de la estación mínima entonces cambia a la emisora más alta 
+            if(stationAM<530){
+                stationAM=1610;
+            }
+        }else{
+            //Si es FM entonces retrocede a la emisora anterior con -0.2
+            stationFM = stationFM-0.2;
+            //Si baja más de la estación mínima entonces cambia a la emisora más alta 
+            if(stationFM<87.9){
+                stationFM=107.9;
+            }
+        }
         
     }
 
     @Override
     public double getStation() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    @Override
-    public void saveStation(int position, double station) {
+        //En este caso se regresa la estación de emisora dependiendo de la frecuencia en la que esté
+        //ya sea AM o FM
+        if(frequency){
+            
+            return stationAM;
+        }else{
+            stationFM=(double)Math.round(stationFM * 100d) / 100d;
+            return stationFM;
+        }
         // TODO Auto-generated method stub
         
     }
 
     @Override
+    public void saveStation(int position, double station) {
+        // TODO Auto-generated method stub
+        //Revisa si es de los botones de FM
+        if(station<108.0&&station>87.6){
+            //Si es FM los almacena en uno de los botones de FM
+            savedStations[position]=station;
+            
+        //Revisa si es de los botones de AM
+        }else if(station<1611&&station>529){
+
+            //Si es AM los almacena en uno de los botones de AM
+            savedStations[position]=station;
+            
+        }
+
+    }
+
+    @Override
     public double getSavedStation(int position) {
         // TODO Auto-generated method stub
-        return 0;
+        //revisa la posición para saber si es FM
+        if(position>11){
+
+           //Al momento de apachar el botón esa emisora empieza a sonar en la frecuencia en la que esté
+            stationFM=savedStations[position];
+
+        //revisa la posición para saber si es AM
+        }else if(position<12){
+
+            //Al momento de apachar el botón esa emisora empieza a sonar en la frecuencia en la que esté
+            stationAM=savedStations[position];
+            
+        }
+
+        return savedStations[position];
     }
 
     @Override
     public boolean getFrequency() {
         // TODO Auto-generated method stub
+        //Este método regresa la frecuencia si es AM o FM
         return frequency;
     }
 
     @Override
     public void switchAMFM() {
+        //Este método cambia de frecuencia AM a FM
+        //AM es positivo y FM es negativo
         frequency = !frequency;
         // TODO Auto-generated method stub
         
